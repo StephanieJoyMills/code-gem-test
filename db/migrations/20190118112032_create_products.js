2456cb80ce88d87563be93db437f6520cfd1ef08
StephanieJ10 <mills.stephanie.j@gmail.com>
2019-01-18 11:29:58 -0500

    init migrations and file structure set-up

---
+++ db/migrations/20190118112032_create_products.js
@@ -0,0 +1,53 @@
+exports.up = function(knex, Promise) {
+    return knex.schema
+    .createTableIfNotExists('products', function(table) {
+        table
+            .uuid('id')
+            .defaultTo(knex.raw('uuid_generate_v4()'))
+            .primary();
+        table
+            .string('title')
+            .notNullable()
+            .unique();
+        table
+            .decimal('price',14,2)
+            .notNullable();
+        table
+            .integer('inventory',14,2)
+            .notNullable()
+            .unsigned();
+        table
+            .timestamp('created_at')
+            .notNullable()
+            .defaultTo(knex.fn.now());
+      })
+      .raw(
+        // add update_row_modified_function_()
+        `CREATE OR REPLACE FUNCTION update_row_modified_function_()
+            RETURNS TRIGGER
+            AS
+            $$
+            BEGIN
+            NEW.updated_at = now();
+            RETURN NEW;
+            END;
+            $$
+            language 'plpgsql';
+            `,
+      )
+        .raw(
+        // add updated_at trigger
+        `CREATE TRIGGER row_mod_on_set_slots_trigger_
+            BEFORE UPDATE
+            ON products
+            FOR EACH ROW
+            EXECUTE PROCEDURE update_row_modified_function_();
+            `,
+        );
+  
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema
+    .dropTableIfExists('products')
+};
